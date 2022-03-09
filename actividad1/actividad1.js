9f3ac2fefe8e47011992ddafbfb808b145ca11de
Yessenia Reyes <yessenia.reyes.juarez@gmail.com>
2022-03-08 20:31:04 -0600

    actividad 1

---
+++ actividad1/actividad1.js
@@ -0,0 +1,22 @@
+// Ejercicio 1
+var dato = Number(prompt("Ingresa un número"))
+var resultado = dato * 2
+console.log(resultado)
+
+// Ejercicio 2
+var primerDato = Number(prompt("Ingresa un número"))
+var segundoDato = Number(prompt("Ingresa otro número"))
+var respuesta = primerDato - segundoDato
+console.log(respuesta)
+
+// Ejercicio 3
+var pi = 3.14159
+var r = 5.3
+var area = pi * r ** 2
+console.log(area)
+
+// Ejercicio 4
+var diagonalMayor = 8
+var diagonalMenor = 6
+var area = diagonalMayor * diagonalMenor / 2
+console.log(area)

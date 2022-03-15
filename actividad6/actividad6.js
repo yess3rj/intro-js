594475c9634e60cbc6fc1a7871201123e52a3741
Yessenia Reyes <yessenia.reyes.juarez@gmail.com>
2022-03-14 21:29:51 -0600

    Correcciones. Metodos array.

--- actividad6/actividad6.js
+++ actividad6/actividad6.js
@@ -3,19 +3,19 @@ var animals = ["eagle", "parrot", "monkey", "boar", "lion"];
 var comingSoonAnimals = ["panter", "dragon", "turtle"];
 
 var totalAnimals = animals.concat(comingSoonAnimals);
-totalAnimals;
+console.log(totalAnimals);
 
 // Ejercicio 2
 var arr = [4, 6, 1, 0, 8, 2]
 var nuevo = [4, 6, 1, 0, 8, 2].sort(function(a, b) { return a - b });
-nuevo;
+console.log(nuevo);
 
 // Ejercicio 3
 var animals = ["eagle", "parrot", "monkey", "boar", "lion"];
 animals.push("cow")
-animals;
+console.log(animals);
 
 // Ejercicio 4
 var animals = ["eagle", "parrot", "monkey", "boar", "lion"];
 animals.shift()
-animals;
+console.log(animals);

77ad0226f375cbb23759979af85d0eace9216138
Yessenia Reyes <yessenia.reyes.juarez@gmail.com>
2022-03-15 21:32:09 -0600

    Funciones, no terminado

---
+++ actividad8/actividad8.js
@@ -0,0 +1,26 @@
+// Ejercicio 1 Retornar true o false, buscando una palabra en una oracion.
+function buscar(oracion, palabra) {
+
+}
+
+
+// Ejercicio 2 Sumar elementos
+var numbers = [1, 2, 3, 4, 10, 11];
+
+function suma() {
+
+
+}
+
+
+
+// Ejercicio 3 Verificar si la palabra es palindromo
+var word1 = "madam";
+var word2 = "computadora";
+
+function palindromo(palabra) {
+
+}
+
+
+// Ejercicio 4 Convertir temperaturas. Celsius-Fahrenheit.
